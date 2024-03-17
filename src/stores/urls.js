91d04a99ca666e0487895227ee0b8d63fdef3309
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2024-03-17 11:48:54 +0100

    Add where to conditionate the getDocs

--- src/stores/urls.js
+++ src/stores/urls.js
@@ -1,7 +1,7 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
-import { collection, getDocs, query } from 'firebase/firestore/lite';
-import { db } from '../firebaseConfig';
+import { collection, getDocs, query, where } from 'firebase/firestore/lite';
+import { db, auth } from '../firebaseConfig';
 
 export const useUrlsStore = defineStore('urls', () => {
   const documents = ref([]);
@@ -11,9 +11,15 @@ export const useUrlsStore = defineStore('urls', () => {
       // Create the query
       // query and collection are imported from 'firebase/firestore/lite'
       // db is the data base that we config on the firebaseConfig file.
-      const q = query(collection(db, 'urls'));
+      const q = query(
+        collection(db, 'urls'),
+        where('user', '==', auth.currentUser.uid)
+      );
+      // where() is a filter. It goes inside the query but outside the collection
+      // where() => ('{property name}', '{condition}', '{value to check}')
+      // auth is imported from our firebaseConfig file and get us the current user
 
-      // We call the db
+      // We call the db from our firebaseConfig file
       // getDocs is imported from 'firebase/firestore/lite'
       // We need to pass the query
       const querySnapshot = await getDocs(q);
@@ -27,6 +33,10 @@ export const useUrlsStore = defineStore('urls', () => {
         // It returns an object with all the data in the db
         console.log('doc.id', doc.id);
         console.log('doc.data()', doc.data());
+        documents.value.push({
+          id: doc.id,
+          ...doc.data(),
+        });
       });
     } catch (error) {
       console.log(error);
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,14 @@
   <div>
     <h2 class="mt-4 mx-5">Home</h2>
     <p class="my-2 mx-5">USER: {{ userStore.userData?.email }}</p>
+    <div class="mx-5">
+      URLS:
+      <ul>
+        <li v-for="item of urlsStore.documents" :key="item.id">
+          {{ item.short }}: {{ item.name }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
